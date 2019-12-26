a2b03819594f94470915155c7b3c65b2285e9f4d
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-26 16:46:52 -0500

    Revamp the broken production environment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const htmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+/*    optimization: {
+	minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()]
+    }, */
     module: {
 	rules: [
 	    {
@@ -22,6 +26,14 @@ module.exports = {
 		exclude: /node_modules/,
 		loader: 'eslint-loader',
 		options: {}
+	    },
+	    {
+		// We use the loader supplied by plugin instead of
+		// style-loader here. This enables putting the styles
+		// as a stylesheet in the HTML, as opposed to styles
+		// done via JS in case of style-loader.
+		test: /\.scss$/,
+		use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
 	    }
 	]
     },
@@ -29,6 +41,7 @@ module.exports = {
 	new htmlWebPackPlugin({
 	    template: './src/client/views/index.html',
 	    filename: './index.html'
-	})
+	}),
+	new MiniCssExtractPlugin({filename: '[name].css'}),
     ]
 }
