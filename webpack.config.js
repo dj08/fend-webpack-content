cd2ee5b40030067557356f9c1ff97f7b84f31532
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-26 11:30:02 -0500

    Import existing code to index.js, add babel for translation

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+	rules: [
+	    {
+		// Run all custom JS in project through
+		// babel. Otherwise webpack will move advanced ES
+		// syntaxes to browser distribution and will create
+		// issues there.
+		test: /\.js$/,
+		exclude: /node_modules/,
+		loader: "babel-loader"
+	    }
+	]
+    }
 }
